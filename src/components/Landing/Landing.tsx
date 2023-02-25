064422d7a55395b0fdac81d126870f1acff653e3
Goverdhan Gaur <84145630+goverdhan-gaur@users.noreply.github.com>
2023-02-24 20:20:12 -0500

    Feat: implemented Redux for api data

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -2,21 +2,22 @@ import React, { FunctionComponent } from 'react'
 import * as Styled from './Landing.styled'
 // import useDeviceDetect from '@/hooks/useDeviceDetection'
 import { Heading } from '../Heading/Heading'
-// import useDeviceDetect from '@/hooks/useDeviceDetection'
 import { Form } from '../Form/Form'
+import { useSelector } from 'react-redux'
+import { VisualizationsContainer } from '../VisualizationsContainer/VisualizationsContainer'
 type Props = {
   //
 }
 export const Landing: FunctionComponent<Props> = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const weatherData = useSelector((state: any) => state.data)
+  const { data } = weatherData
+  console.log(data)
   return (
     <Styled.wrapper>
       <Heading />
       <Form />
+      {data && <VisualizationsContainer data={data} />}
     </Styled.wrapper>
   )
-
-  // Heading
-
-  // Form fields
-  // visualization
 }
